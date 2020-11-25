0844c898cbb931e5b70f225e2202f06e7881fc53
justinfernald <jfernald@my.gcu.edu>
2020-11-25 14:24:58 -0700

    Revert "Merge"
    This reverts commit 7fc20eb37f576196ddfbd7b763667cbb27dd9d7e, reversing
    changes made to fa9b24291557ff19431cb365c79fe0b6d735cac6.

--- Assets/Scripts/MakeTransparent.cs
+++ Assets/Scripts/MakeTransparent.cs
@@ -5,34 +5,19 @@ using UnityEngine.UI;
 
 public class MakeTransparent : MonoBehaviour
 {
-    public bool hasCube = false;
-    bool isTransparent = false;
-    float totalTimeSteps = 0;
+    public GameObject currentGameObject;
+    public Slider transparencySlider;
     void Start()
     {
+        currentGameObject = gameObject;
         // capture changes in sliders and change transparency
-        //transparencySlider.onValueChanged.AddListener(delegate { ChangeMaterial(currentGameObject.GetComponent<Renderer>().material, 1); });
+        transparencySlider.onValueChanged.AddListener(delegate { ChangeMaterial(currentGameObject.GetComponent<Renderer>().material, 1); });
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isTransparent) return;
-        totalTimeSteps += Time.deltaTime;
-        ChangeMaterial(gameObject.GetComponent<Renderer>().material, 1f - Mathf.Clamp(totalTimeSteps / 5, 0f, 1f));
-    }
-
-    public void ShowTransparent()
-    {
-        isTransparent = true;
-        totalTimeSteps = 0;
-    }
-
-    public void RemoveTransparent()
-    {
-        isTransparent = false;
-        totalTimeSteps = 0;
-        ChangeMaterial(gameObject.GetComponent<Renderer>().material, 1f);
+        ChangeMaterial(currentGameObject.GetComponent<Renderer>().material, 1f-transparencySlider.value);
     }
 
     void ChangeMaterial(Material mat, float alphaVal)
